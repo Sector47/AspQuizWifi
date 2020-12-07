f1cd336504606550242cce5dcb7ff733eaf809f9
Tyler Hammerseng <21712472@nwtc.edu>
2020-12-07 04:11:42 -0600

    Modify questions just needs to filter according to quiz id

--- Site/Models/QuestionData.cs
+++ Site/Models/QuestionData.cs
@@ -9,11 +9,22 @@ namespace Site.Models
     {
         public string que_ID { get; set; }
         public string que_question { get; set; }
+
         public string type_ID { get; set; }
+        public ANS_TYPE typeID{ get; set; }
 
         public List<int> ans_IDList { get; set; }
         public List<string> descriptionList { get; set; }
+
         //public string[,] questionData { get; set; }
         //public string[,] answerData { get; set; }
+    }
+
+    public enum ANS_TYPE
+    {
+        FB,
+        MCC,
+        MCR,
+        SA
     }
 }
---
+++ Site/Models/QuizData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site.Models
+{
+    public class QuizData
+    {
+        public int QUI_NAME { get; set; }
+        public string QUI_NOTES { get; set; }
+        public int QUI_ID { get; set; }
+    }
+}

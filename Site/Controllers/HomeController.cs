ccdd19e4b3d624977dd2dd0dbc8bbd4cf2f341ec
Johnathan DeLeeuw <john.m.deleeuw@gmail.com>
2020-12-07 03:58:46 -0600

    Added methods to database creator to handle grading and submitting quizzes ++AddGradeDB() adds a row to grade table with specified values ++AddResponseDB() adds rows to response table with specified values for each response in the list ++gradeQuizDB() grades the specified quiz_id against the list of response. Then calls addGradeDB() and AddResponseDB() to add the grades and responses to the table.
    Have the controller action for submit quiz call gradeQuizDB() then redirect the user to a page showing the grade afterwards.
    
    Rewrote parts of the homecontroller to change data types of a few variables from string[,] to list<string[]> for easier access and to avoid null array rows
    
    Added question count to quiz page, correctly set values of form items in the question type_ID partial views to be able to get the normal response values in the controller instead of the randomized values that are displayed.

--- Site/Controllers/HomeController.cs
+++ Site/Controllers/HomeController.cs
@@ -123,40 +123,41 @@ namespace Site.Controllers
             // Make our list to hold our questiondata objects we will create
             List<QuestionData> questionDataList = new List<QuestionData>();
             // Grab the string 2d arrays of the questions and answers for the given quizID
-            string[,] questionDataArray = databaseCreator.getQuestionDataDB(quizID);
+            List<string[]> questionList = databaseCreator.getQuestionDataDB(quizID);
             // for getting answer data we need to know all the questionIDs for that quiz, so we make a list of them to pass through.
             List<int> questionIDList = databaseCreator.getQuestionIDsDB(quizID);
-            string[,] answerData = databaseCreator.getAnswerDataDB(questionIDList);
+            List<string[]> answerData = databaseCreator.getAnswerDataDB(questionIDList);
             // after we have the data we need we can create our questionData object for each item 
             // loop through our questionData 2d array
-            for (int i = 0; i < questionDataArray.GetLength(0); i++)
+            foreach(string[] q in questionList)
             {
                 // make our temporary ans_ID list to be able to add it to our question data object
                 List<int> tempAns_IDList = new List<int>();
-                for (int j = 0; j < answerData.GetLength(0); j++)
-                {
-                    if (answerData[j, 1] == questionDataArray[i, 0])
-                        tempAns_IDList.Add(System.Convert.ToInt32(answerData[j, 0]));
-                }
 
                 // make our temporary description list to be able to add it to our question data object
                 List<string> tempDescriptionList = new List<string>();
-                for (int k = 0; k < answerData.GetLength(0); k++)
+                //"SELECT ANS_ID, QUE_ID, DESCRIPTION FROM ANSWER WHERE "; QUE_ID, QUE_QUESTION, TYPE_ID, QUESTION_ANSWER
+                // Loop through our list of answer data and place them into our temporary lists if they should be attached to the current question.(ans_id/fk match)
+                foreach (string[] a in answerData)
                 {
-                    if (answerData[k, 1] == questionDataArray[i, 0])
-                        tempDescriptionList.Add(answerData[k, 2]);
+                    if (a[1] == q[0])
+                    {
+                        tempAns_IDList.Add(System.Convert.ToInt32(a[0]));
+                        tempDescriptionList.Add(a[2]);
+                    }
                 }
 
-                // Create our questionData object and add it to the QuestionDataList
-                questionDataList.Add(new QuestionData
-                {
-                    que_ID = questionDataArray[i, 0],
-                    que_question = questionDataArray[i, 1],
-                    type_ID = questionDataArray[i, 2],
-                    ans_IDList = tempAns_IDList,
-                    descriptionList = tempDescriptionList
-                });
+                // Create our questionData object and add it to the QuestionDataList if it isn't null
+                    questionDataList.Add(new QuestionData
+                    {
+                        que_ID = q[0],
+                        que_question = q[1],
+                        type_ID = q[2],
+                        ans_IDList = tempAns_IDList,
+                        descriptionList = tempDescriptionList
+                    });
             }
+
             ViewBag.QuizName = databaseCreator.getQuizNameDB(quizID);
             ViewBag.Quiz_ID = quizID;
             // pass our new question data object to the quiz view.
@@ -219,26 +220,24 @@ namespace Site.Controllers
         }
 
         [HttpPost]
-        public ActionResult SubmitQuiz(string qui_ID)
+        public ActionResult SubmitQuiz(int qui_ID)
         {
             DatabaseCreator databaseCreator = new DatabaseCreator();
-
-            string name = Request.Form["1"];
-            string two = Request.Form["2"];
-            string three = Request.Form["3"];
-            string four = Request.Form["4"];
-            string five = Request.Form["5"];
-            string six = Request.Form["6"];
-            string seven = Request.Form["7"];
+            List<string> responseList = new List<string>();
+            List<int> questionList = databaseCreator.getQuestionIDsDB(qui_ID);
 
 
+            foreach (int i in questionList)
+            {
+                responseList.Add(Request.Form[i.ToString()]);
+            }
 
+            int grade = databaseCreator.gradeQuizDB(qui_ID, responseList, getLoggedInUserID(), databaseCreator.getCourseQuizIDDB(qui_ID, getLoggedInUserID()));
             //ViewBag.QuizName = databaseCreator.getQuizNameDB(quiz_ID);
             // TODO calculate grade
             // TODO update grade table
-            string grade = "10/10";
             ViewBag.Grade = grade;
-            return View("Quiz");
+            return View("Quiz", grade);
         }
 
         // Change the password of the current user. 
